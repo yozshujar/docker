6947ff46c2caddbe50eedc67c7c878ad923b21c5
yozshujar <baralgin.andrey@gmail.com>
2018-04-03 12:55:17 +0300

    added docker-compose

--- docker.sh
+++ docker.sh
@@ -2,4 +2,4 @@
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 echo 'deb https://download.docker.com/linux/debian stretch stable' > /etc/apt/sources.list.d/docker.list
 apt update -y
-apt install docker-ce -y
+apt install docker-ce docker-compose -y
