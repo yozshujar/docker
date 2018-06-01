5b237a49936e3033dcb37d7fe968a81e3f61f66c
yozshujar <baralgin.andrey@gmail.com>
2018-06-01 12:54:06 +0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-echo 'deb https://download.docker.com/linux/debian stretch stable' > /etc/apt/sources.list.d/docker.list
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
 apt update -y
-apt install docker-ce docker-compose -y
+apt install -y docker-engine
